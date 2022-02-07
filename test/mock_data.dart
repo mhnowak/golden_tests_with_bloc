74ab5b8bb45659265785d42b669d7ddd0cdc82f5
Michał Nowak <michal.nowak@appunite.com>
2022-02-07 11:07:21 +0100

    bloc part of riverpod vs bloc article

---
+++ test/mock_data.dart
@@ -0,0 +1,16 @@
+import 'package:golden_tests_with_bloc/model/winners_model.dart';
+
+const winnersModel = WinnersModel(
+  firstPlace: Driver(
+    firstName: 'Max',
+    familyName: 'Verstappen',
+  ),
+  secondPlace: Driver(
+    firstName: 'Lewis',
+    familyName: 'Hamilton',
+  ),
+  thirdPlace: Driver(
+    firstName: 'Carlos',
+    familyName: 'Sainz',
+  ),
+);
